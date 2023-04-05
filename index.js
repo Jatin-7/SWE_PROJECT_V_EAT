8306404761bce53795167ff4ef6311f16478e6a4
Jatin Singh <71443622+Jatin-7@users.noreply.github.com>
2023-04-05 12:30:06 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const connectDB = require("./utils/connectDB");
+const bodyParser = require("body-parser");
+const session = require("express-session");
+const cors = require("cors");
+
+const app = express();
+app.use(bodyParser.json());
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(cors());
+connectDB();
+
+app.use(
+  session({
+    secret: "secret key",
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+
+app.use(express.static(__dirname + "/assets"));
+
+// View Engine
+app.set("view engine", "ejs");
+app.set("views", "./views");
+
+app.use("/restaurant", require("./routes/RestaurantRoutes"));
+app.use("/menu", require("./routes/MenuRoutes"));
+app.use("/customer", require("./routes/CustomerRoutes"));
+app.use("/owner", require("./routes/OwnerRoutes"));
+app.use("/order", require("./routes/OrderRoutes"));
+
+app.get("/", (req, res) => {
+  res.render("index.ejs", { msg: "" });
+});
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log("Server started");
+});
